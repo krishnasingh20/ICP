3b315a1144266964f0de8f547fe008f5126e6284
Krishna Singh <krishnasinghks1225@gmail.com>
2025-10-27 23:30:07 +0530

    Add Next Greater Element I solution
    Implement next greater element algorithm using a stack and hashmap.

---
+++ Assignment10/Next_Greater_Element_I.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], i);
+        }
+        Stack<Integer> st = new Stack<>();
+        for(int i = 0; i < nums2.length; i++) {
+            while(!st.isEmpty() && nums2[st.peek()] < nums2[i]) {
+                nums2[st.pop()] = nums2[i];
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty()) {
+            nums2[st.pop()] = -1;
+        }
+        for(int i = 0; i < nums1.length; i++) {
+            nums1[i] = nums2[map.get(nums1[i])];
+        }
+        return nums1;
+    }
+}
+// TC--> O(N+M)
+// SC--> O(N)
